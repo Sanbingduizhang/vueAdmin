14eef416941e8de25f2b7e4abd03e6f7901d59a1
冯鑫龙 <18862005869@163.com>
2018-09-14 16:12:20 +0800

    2
    2

---
+++ app/Modules/Home/Http/Controllers/SelfController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Modules\Home\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+class SelfController extends Controller
+{
+    //显示个人作品
+    public function index()
+    {
+
+
+    }
+}
